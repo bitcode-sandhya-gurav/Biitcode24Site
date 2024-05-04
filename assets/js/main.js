ac44967ee07f4edbd5adba58288e99fe9551f8c3
bitcode-sandhya-gurav <sandhya.gurav@bit-code.in>
2024-05-04 19:19:41 +0530

    Added changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,7 @@
 	////////////////////////////////////////////////////
 	// 01. PreLoader Js
 	windowOn.on('load', function () {
-		$("#loading").fadeOut(500);
+		$("#loading").fadeOut(100);
 	});
 
 	$('#section-time').onePageNav({
